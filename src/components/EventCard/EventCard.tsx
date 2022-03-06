2c3b6f72250ed7574492850377fc10dc2f46f4ea
gabdonada <48109393+gabdonada@users.noreply.github.com>
2022-03-06 00:54:24 -0300

    Updating event card

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,3 +1,5 @@
+import moment from "moment"
+
 type Card = {
     id: string,
     titulo: string,
@@ -8,21 +10,32 @@ type Card = {
 export function EventCard(props: {props: Card}){
     return(
         <a href={`/Evento/${props.props.id}`} className="text-decoration-none link-dark">
-            <div className="d-flex card mb-3">
-                <div className="d-flex flex-column align-items-center">
-                    
-                        <h1>31</h1>
-                    
-                        <h1>Dec</h1>
-                    
-                </div>
-                <div>
-                    {props.props.titulo}
-                </div>
-                <div>
-                    Confirmados
+            <div className="card mb-3 pt-1">
+                <div className="d-flex w-100">
+                    <div className="d-flex flex-column align-items-center w-25">
+                        
+                            <h1>{moment(props.props.dataInicio).format("DD")}</h1>
+                        
+                            <h1>{moment(props.props.dataInicio).format("MMMM")}</h1>
+                        
+                    </div>
+
+                    <div className="d-flex flex-column justify-content-center w-50">
+                        <div></div>
+                        <h1>
+                            {props.props.titulo}
+                        </h1>
+                    </div>
+
+                    <div className="d-flex flex-column justify-content-center w-25">
+                        <div></div>
+                        <h1>
+                            Confirmados
+                        </h1>
+                    </div>
                 </div>
             </div>
+            
         </a>
         
     )
