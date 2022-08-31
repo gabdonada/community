bf57c8bf33d3b3a3de174be1f0440c5d6c262338
Gabriel Donada <gabriel.donada@sap.com>
2022-08-30 23:52:30 -0300

    Adding tools to enable user to update their profiles

--- src/pages/userProfile/visualizarProfile/EditUserProfile.tsx
+++ src/pages/userProfile/visualizarProfile/EditUserProfile.tsx
@@ -21,15 +21,15 @@ export function EditUserProfile(){
     const navigate = useNavigate();
 
 
-    const [ description, setDescription ] = useState(userDef?.userDescription ?? '')
-    const [ phone, setPhone ] = useState(userDef?.userPhone ?? '')
-    const [ title, setTitle ] = useState(userDef?.userTitle ?? '')
-    const [ city, setCity ] = useState(userDef?.userCity ?? '')
+    const [ description, setDescription ] = useState( '')
+    const [ phone, setPhone ] = useState('')
+    const [ title, setTitle ] = useState('')
+    const [ city, setCity ] = useState('')
 
-    const [ acRelacaoFins, setAcRelacaoFins ] = useState<boolean>(userDef?.userInterests.acRelacaoFins ?? false)
-    const [ acRelacaoValores, setAcRelacaoValores ] = useState<boolean>(userDef?.userInterests.acRelacaoValores ?? false)
-    const [ acAfetiva, setAcAfetiva ] = useState<boolean>(userDef?.userInterests.acAfetiva ?? false)
-    const [ acTradi, setAcTradi ] = useState<boolean>(userDef?.userInterests.acTradi ?? false)
+    const [ acRelacaoFins, setAcRelacaoFins ] = useState<boolean>(false)
+    const [ acRelacaoValores, setAcRelacaoValores ] = useState<boolean>(false)
+    const [ acAfetiva, setAcAfetiva ] = useState<boolean>(false)
+    const [ acTradi, setAcTradi ] = useState<boolean>(false)
 
     const [ changeDesc, setChangeDesc] = useState(false)
     const [ changeTit, setChangeTit] = useState(false)
@@ -120,39 +120,39 @@ export function EditUserProfile(){
                                 type="text"
                                 className="form-control mb-3" 
                                 onChange={event => {setTitle(event.target.value); handleChangeTitle()}}
-                                value={title || userDef?.userTitle}/>
+                                value={title || changeTit ? title : changeTit ? title : userDef?.userTitle}/>
 
                             <label className="form-label">Telefone</label>
                             <input 
                                 type="text"
                                 className="form-control mb-3" 
                                 onChange={event => {setPhone(event.target.value); handleChangePhone()}}
-                                value={phone || userDef?.userPhone}/>
+                                value={phone || changePho ? phone : userDef?.userPhone}/>
                             
                             <label className="form-label">Selecione seus interesses</label>
                             <div className='d-flex gap-2 mb-2 d-flex align-items-center'>
-                                <input className="form-check-input" type="checkbox" defaultChecked={acRelacaoFins || userDef?.userInterests.acRelacaoFins} onChange={event => {acRelacaoFins === false ? setAcRelacaoFins(true):setAcRelacaoFins(false); handleChangeARF()}}/>
+                                <input className="form-check-input" type="checkbox" defaultChecked={acRelacaoFins || changeARF ? acRelacaoFins : userDef?.userInterests.acRelacaoFins} onChange={event => {acRelacaoFins === false ? setAcRelacaoFins(true):setAcRelacaoFins(false); handleChangeARF()}}/>
                                 <label className="form-check-label">
                                     Ação social racional com relação a fins
                                 </label>
                             </div>
 
                             <div className='d-flex gap-2 mb-2 d-flex align-items-center'>
-                                <input className="form-check-input" type="checkbox" defaultChecked={acRelacaoValores || userDef?.userInterests.acRelacaoValores} onChange={event => {acRelacaoValores === false ? setAcRelacaoValores(true):setAcRelacaoValores(false); handleChangeARV()}}/>
+                                <input className="form-check-input" type="checkbox" defaultChecked={acRelacaoValores || changeARV ? changeARV : userDef?.userInterests.acRelacaoValores} onChange={event => {acRelacaoValores === false ? setAcRelacaoValores(true):setAcRelacaoValores(false); handleChangeARV()}}/>
                                 <label className="form-check-label">
                                     Ação social racional com relação a valores
                                 </label>
                             </div>
                             
                             <div className='d-flex gap-2 mb-2 d-flex align-items-center'>
-                                <input className="form-check-input" type="checkbox" defaultChecked={acAfetiva || userDef?.userInterests.acAfetiva} onChange={event => {acAfetiva === false ? setAcAfetiva(true):setAcAfetiva(false); handleChangeAA()}}/>
+                                <input className="form-check-input" type="checkbox" defaultChecked={acAfetiva || changeAA ? changeAA : userDef?.userInterests.acAfetiva} onChange={event => {acAfetiva === false ? setAcAfetiva(true):setAcAfetiva(false); handleChangeAA()}}/>
                                 <label className="form-check-label">
                                     Ação social afetiva
                                 </label>
                             </div>
                             
                             <div className='d-flex gap-2 mb-3 d-flex align-items-center'>
-                                <input className="form-check-input" type="checkbox" defaultChecked={acTradi || userDef?.userInterests.acTradi} onChange={event => {acTradi === false ? setAcTradi(true):setAcTradi(false); handleChangeAT()}}/>
+                                <input className="form-check-input" type="checkbox" defaultChecked={acTradi || changeAT ? changeAT : userDef?.userInterests.acTradi} onChange={event => {acTradi === false ? setAcTradi(true):setAcTradi(false); handleChangeAT()}}/>
                                 <label className="form-check-label">
                                     Ação social tradicional
                                 </label>
@@ -163,13 +163,13 @@ export function EditUserProfile(){
                                 type="text"
                                 className="form-control mb-3" 
                                 onChange={event => {setCity(event.target.value); handleChangeCi()}}
-                                value={city || userDef?.userCity}/>
+                                value={city || changeCi ? city : userDef?.userCity}/>
 
                             <label className="form-label">Escreva sobre você</label>
                             <textarea 
                                 className="form-control mb-3" 
                                 onChange={event => {setDescription(event.target.value); handleChangeDesc()}}
-                                value={description || userDef?.userDescription}></textarea>
+                                value={description || changeDesc ? description : userDef?.userDescription}></textarea>
 
                             <Button type='submit'>Salvar</Button>
                         </form>
